8e097735b15853856a389fc4cc4028f3bdcd1f04
Prénom NOM <juliette.narquin@ens.uvsq.fr>
2021-02-19 10:08:31 +0100

    fg

---
+++ incendie.py
@@ -0,0 +1,9 @@
+#########################################
+# groupe BI 4 
+# Perrine ILARY 
+# Romane GENSE
+# Romane DONAT-GROS-JEAN
+# Sabah KEBLI
+# Juliette NARQUIN
+# https://github.com/uvsq22002160/projet_incendie/
+#########################################
